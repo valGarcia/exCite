533c615c3fbf5fc85119dc7194765b35ace90683
valGarcia <valerie.garcia01@gmail.com>
2015-10-24 10:15:45 -0700

    now we have function parselinks

--- js/app.js
+++ js/app.js
@@ -44,8 +44,8 @@ function onAnchorClick(event){
 	return false;
 }
 
-function submitted(event){
-	console.log(form);
+function parselinks(arr){
+	console.log(arr);
 }
 
 //given an array of URLs, build a DOM list of those URLs 
@@ -94,22 +94,24 @@ function buildPopopDom(divName, data){
 	submitelement.setAttribute("name", "dsubmit");
 	submitelement.setAttribute("value", "Submit");
 	form.appendChild(submitelement);
-	var linksToCite = $(submitelement).on("click", function(e){	
+	
+	var listofLinks = [];
+	$(submitelement).on("click", function(e){	
 		e.preventDefault();
-		var listofLinks = [];
 		var children = form.childNodes; 
 		for (i = 0, ie = children.length; i < ie; ++i) {
 			if(children[i].localName == "input" && children[i].checked)
 			{
-				listofLinks.push(children[i].value);
+				listofLinks.push(getCiteData(children[i].value));
+//				console.log(listofLinks[listofLinks.length -1]);			
 			}
 		}
-		return listofLinks;
+		parselinks(listofLinks);
+		// return listofLinks;
 	});
-	
-	var url = "http://www.nytimes.com/2015/10/24/opinion/reinventing-the-library.html"
-	console.log(getCiteData(url));
-	
+	// for(var k = 0, ke = listofLinks.length; k < ke; k++){
+	// 	console.log(listofLinks[k]);
+	// }
 }
 
 
